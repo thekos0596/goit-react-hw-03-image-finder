0ce7f3749b4cfccc9a2a67ba6fc2ff349b8628b9
Haidai Kostiantyn <thekos0596@gmail.com>
2023-04-22 14:36:00 +0300

    update

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,52 @@
+import { Component } from 'react';
+import { toast } from 'react-toastify';
+import PropTypes from 'prop-types';
+import {
+  Header,
+  SearchForm,
+  SearchFormBtn,
+  SearchFormInput,
+} from './Searchbar.styled';
+
+export default class Searchbar extends Component {
+  state = { query: '' };
+
+  hendleSearchChange = event => {
+    this.setState({ query: event.currentTarget.value.toLowerCase() });
+  };
+
+  hendleSubmit = event => {
+    event.preventDefault();
+
+    if (this.state.query.trim() === '') {
+      toast.warn('Please enter Search Request !', {
+        position: toast.POSITION.TOP_CENTER,
+      });
+      return;
+    }
+
+    this.props.onSubmit(this.state.query);
+    this.setState({ query: '' });
+  };
+
+  render() {
+    return (
+      <Header className="searchbar">
+        <SearchForm className="form" onSubmit={this.hendleSubmit}>
+          <SearchFormBtn type="submit">Search</SearchFormBtn>
+
+          <SearchFormInput
+            className="input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={this.hendleSearchChange}
+          />
+        </SearchForm>
+      </Header>
+    );
+  }
+}
+
+Searchbar.propTypes = { onSubmit: PropTypes.func.isRequired };
